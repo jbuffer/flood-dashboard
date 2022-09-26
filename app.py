71eb2fa4d80b547c2219dfb0abcec72b73492de4
jbuffer <jb331@student.london.ac.uk>
2022-09-26 20:59:19 +0100

    added sparklines and refactored

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-# import relevant files
-from venv import create
+from ast import literal_eval
 import os
 import streamlit as st
-import numpy as np
 import pandas as pd
 from streamlit_folium import folium_static
-import requests
-from datetime import datetime, date
-from src.utils import create_map, get_coord
+from datetime import date
+from src.utils import create_map, sparkline
 
 # making wide layout default
 st.set_page_config(layout="wide")
@@ -15,9 +12,9 @@ st.set_page_config(layout="wide")
 "# Flood Data from the Environment Agency"
 
 # get source data
-df = pd.read_csv('https://raw.githubusercontent.com/jbuffer/flood-actions/main/data/flood-data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/jbuffer/flood-actions/main/data/flood-data.csv') # noqaE501
 
-#format date and set parameters
+# format date and set parameters
 df['date'] = pd.to_datetime(df['date']).dt.date
 max_date = date.today()
 min_date = df['date'].min()
@@ -25,68 +22,45 @@ min_date = df['date'].min()
 # add sidebar logo and input
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'blue_logo/logo_water.png')
-st.sidebar.image(filename, caption='MilaAdam, CC BY-SA 4.0, via Wikimedia Commons')
-
-# sidebar postcode input
-st.sidebar.write('''
-**Postcode** \n
-Enter a postcode in the "Postcode finder" widget 
-to change the focus of the map
-''')
-
-try:
-    latlon = st.sidebar.text_input('Postcode finder:', value='SW1A 2AA', max_chars=8, key=None, type='default')
-    r = requests.get('https://api.postcodes.io/postcodes/{}'.format(latlon))
-    lat = r.json()['result']['latitude']
-    lon = r.json()['result']['longitude']
-    lsoa = r.json()['result']['lsoa']
-    
-except:
-    st.sidebar.write('*This is not a valid postcode. Please try again* :sunglasses:')
-    r = requests.get('https://api.postcodes.io/postcodes/{}'.format('WC1B3HF'))
-    lat = r.json()['result']['latitude']
-    lon = r.json()['result']['longitude']
-    lsoa = r.json()['result']['lsoa']
+st.sidebar.image(filename, caption='MilaAdam, CC BY-SA 4.0, via Wikimedia Commons') # noqaE501
 
 # sidebar date
-st.sidebar.write(f''' 
+st.sidebar.write(f'''
 **Date selection** \n
 Historical data goes back to {min_date}
 ''')
-date_in= st.sidebar.date_input('Select date of interest', 
-    min_value=min_date,
-    max_value=max_date, 
-    value=date.today())
+date_in= st.sidebar.date_input('Select date of interest', min_value=min_date, max_value=max_date, value=date.today()) # noqaE501
 
 df = df[df['date'] == date_in]
 df.reset_index(inplace=True, drop=True)
 
-if df['flood_area_id'].hasnans == True:
+if df['flood_area_id'].hasnans is True:
     # create map
-    m = create_map(lat, lon, df = None)
+    m = create_map(52.486244, -1.890401, df=None)
     st.header('No floods or recent flooding on this date')
-    
+
 else:
-    coords_list = map(get_coord, df['polygon_url'])
-    df['coords'] = list(coords_list) 
+    # create options
+    option = st.selectbox('Select your flooded area of interest:', (df.description)) # noqaE501
+    lat = df['lat'][df['description'] == option][0]
+    lon = df['long'][df['description'] == option][0]
+    st.write(f'''You selected an area in:
+    {df['county'][df['description'] == option][0]} County''')
+
+    # apply create_map function for mapping
+    df['coordinates'] = df['coords'].apply(literal_eval)
     m = create_map(lat, lon, df)
 
-    table_df = df[['county', 'severity', 'riverorsea', 'description' ]]
+    # apply sparklines function for table
+    df['area'] = df['coordinates'].apply(sparkline)
+    table_df = df[['county', 'severity', 'riverorsea', 'description', 'area']]
     table_df = table_df.drop_duplicates()
-    table_df.rename(columns={
-    'County':'county',
-    'Warning': 'severity',
-    'River or Sea': 'riverorsea',
-    'Description': 'description'
-    })
 
     with st.expander('Details', True):
-        st.table(table_df)
-
+        st.write(table_df.to_html(escape=False, index=False), unsafe_allow_html=True) # noqaE501
 
 # call to render Folium map in Streamlit
 
-st.write("Use the 'Postcode finder' widget in the sidebar to focus on a place, or zoom out to see a country-wide view:")
+st.write("Use the 'Postcode finder' widget in the sidebar to focus on a place, or zoom out to see a country-wide view:") # noqaE501
 # add map
 folium_static(m)
-
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,17 @@
+import base64
+from io import BytesIO
+import folium
+from matplotlib import pyplot as plt
 
-import folium 
-import requests
 
-def get_coord(x):
-    r3 = requests.get(x).json()
-    coords = r3['features'][0]['geometry']
-    return coords
-
-def create_map(lat, lon, df):
+def create_map(lat, lon, df, zoom=12):
     m = folium.Map(location=[lat, lon],
-                min_zoom=7, 
-                max_zoom=16,
-                zoom_start=12)
+                   min_zoom=7,
+                   max_zoom=16,
+                   zoom_start=13)
 
     folium.TileLayer('cartodb dark_matter').add_to(m)
 
-    style_0 = {'fillColor': '#2ca25f',  'color': '#2ca25f', "fillOpacity": 0.1, "weight": 1.7}
     style_1 = {'fillColor': '#dd1c77',  'color': '#dd1c77', "fillOpacity": 0.5}
     style_2 = {'fillColor': '#bdbdbd',  'color': '#756bb1', "fillOpacity": 0.5}
 
@@ -24,27 +20,49 @@ def create_map(lat, lon, df):
         flood = folium.FeatureGroup(name='Flooded area', show=True)
         m.add_child(flood)
 
-        flood_no = folium.FeatureGroup(name='Warning no longer active', show=True)
+        flood_no = folium.FeatureGroup(name='Warning no longer active', show=True) # noqaE501
         m.add_child(flood_no)
 
-        warning_df = df[df['severity']=='Flood warning']
+        warning_df = df[df['severity'] == 'Flood warning']
         warning_df.reset_index(inplace=True, drop=True)
 
-        no_df = df[df['severity']!='Flood warning']
+        no_df = df[df['severity'] != 'Flood warning']
         no_df.reset_index(inplace=True, drop=True)
 
         for i in range(len(warning_df)):
-            geo_json = folium.GeoJson(warning_df['coords'][i], style_function = lambda x:style_1)
-            geo_json.add_child( folium.Popup('Status: {} \n Description: {} \n Severity: {}' .format(warning_df['severity'][i],
-            warning_df['description'][i], warning_df['severity_level'][i])))
+            geo_json = folium.GeoJson(warning_df['coordinates'][i], style_function = lambda x:style_1) # noqaE501
+            geo_json.add_child(folium.Popup('Status: {} \n Description: {} \n Severity: {}' .format(warning_df['severity'][i], # noqaE501
+                                            warning_df['description'][i],
+                                            warning_df['severity_level'][i])))
             geo_json.add_to(flood)
 
         for i in range(len(no_df)):
-            geo_json2 = folium.GeoJson(no_df['coords'][i], style_function = lambda x:style_2)
-            geo_json2.add_child( folium.Popup('Status: {} \n Description: {} \n Severity: {} \n Date changed: {}' .format(no_df['severity'][i],
-            no_df['description'][i], no_df['severity_level'][i], no_df['time_changed'][i])))
+            geo_json2 = folium.GeoJson(no_df['coordinates'][i], style_function=lambda x:style_2) # noqaE501
+            geo_json2.add_child( folium.Popup('Status: {} \n Description: {} \n Severity: {} \n Date changed: {}' .format(no_df['severity'][i], # noqaE501
+            no_df['description'][i], no_df['severity_level'][i], no_df['time_changed'][i]))) # noqaE501
             geo_json2.add_to(flood_no)
-    
-    folium.LayerControl(collapsed = False).add_to(m)
+
+    folium.LayerControl(collapsed=False).add_to(m)
 
     return (m)
+
+
+def sparkline(data, figsize=(1.5, 0.75), **kwargs):
+    """
+    creates a sparkline
+    """
+
+    data = data['coordinates'][0]
+    xs, ys = zip(*data)  # create lists of x and y values
+
+    *_, ax = plt.subplots(1, 1, figsize=figsize, **kwargs)
+    ax.plot(xs, ys, color='navy')
+    ax.fill(xs, ys, color='navy', alpha=0.5)
+    ax.axis('off')
+
+    # save the figure to html
+    bio = BytesIO()
+    plt.savefig(bio, transparent=True)
+    plt.close()
+    html = """<img src="data:image/png;base64,%s"/>""" % base64.b64encode(bio.getvalue()).decode('utf-8') # noqaE501
+    return html
